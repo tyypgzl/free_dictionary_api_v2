4c94b4891600b64f681b756800dd11462090fa0a
tayyipguzel <tguzeldev@gmail.com>
2024-02-25 12:40:34 +0300

    fix: example  was edited

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,11 +1,13 @@
+import 'dart:developer' as dev;
+
 import 'package:free_dictionary_api_v2/free_dictionary_api_v2.dart';
 
 Future<void> main() async {
   try {
     final dictionary = FreeDictionaryApiV2();
-    final response = await dictionary.getDefinition('hello', langCode: 'tr');
-    print(response);
+    final response = await dictionary.getDefinition('hello');
+    dev.log(response.toString());
   } on FreeDictionaryException catch (error, stackTrace) {
-    print('Exception: $error stackTrace: $stackTrace');
+    dev.log('FreeDictionaryException:', error: error, stackTrace: stackTrace);
   }
 }
